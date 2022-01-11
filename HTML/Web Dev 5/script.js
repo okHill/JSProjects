122aef4c80906783486cd35fb6b22b93d78066fe
okHill <okurhilmi@gmail.com>
2022-01-11 11:54:33 -0600

    third commit

---
+++ HTML/Web Dev 5/script.js
@@ -0,0 +1,27 @@
+// console.log('Hello World');
+// console.log('JavaScript!!!');
+// console.log(11);
+// // print SoftInnovas on Console
+// console.log('SoftInnovas');
+// // This is my name
+// console.log('Hilmi');
+// /* This is my favorite number. */
+// console.log(6);
+
+var myName = 'Ali';
+
+// First letter should not be a number 3myName = 'Ali'
+// no space between words my Name = 'Ali'
+// var names are case sensitive myName != myname
+console.log(myName);
+console.log(myName);
+
+var myNum = 3.14;
+var lastName = 'Okur';
+var isActive = true;
+
+console.log(myNum);
+console.log(isActive);
+console.log(lastName);
+
+
